00acdb6b239a0a27975065667102cf29c6bfc526
Ikechukwu Chiemelie Charles <chiemeliecharleschristopher@gmail.com>
2024-12-22 05:01:31 +0100

    linked pages

--- ham.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobile-menu");
-
-hamburger.addEventListener("click", () => {
-  // Toggle active classes for animation
-  hamburger.classList.toggle("open");
-  mobileMenu.classList.toggle("open");
-});
